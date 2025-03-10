8a3baae67a50f7e04f15af6ba29325f9fc3821f1
Steve Cirelli <scirelli+git@gmail.com>
2025-03-10 09:42:21 -0400

    Playing with ways to create elements

--- index.js
+++ index.js
@@ -1,18 +1,89 @@
 const ws = new WebSocket('ws://localhost:3000');
 
-function createSliderGroup() {
+function createSliderGroup(id) {
 	let fsElm = document.createElement('fieldset'),
 		legendElm = document.createElement('legend'),
+		
+		syncCheckbox	 	 = document.createElement('input'),
 
-		slider1LabelElm = document.createElement('label'),
-		slider1Elm = document.createElement('input'),
-		slider1ValueElem = document.createElement('span'),
+		slider1LabelElm  = document.createElement('label'),
+		slider1Elm		 	 = document.createElement('input'),
+		slider1ValueElm  = document.createElement('span'),
 
-		slider2LabelElm = document.createElement('label'),
-		slider2Elm = document.createElement('input'),
-		slider2ValueElem = document.createElement('span');
+		slider2LabelElm  = document.createElement('label'),
+		slider2Elm       = document.createElement('input'),
+		slider2ValueElm  = document.createElement('span');
+
+		syncCheckbox.type = 'checkbox';
+		syncCheckbox.checked = true;
+		legendElm.appendChild(syncCheckbox);
+		legendElm.append(document.createTextNode('Linked'));
 
 	  fsElm.classList.add('ir-group');
+		fsElm.appendChild(legendElm);
+
+		fsElm.appendChild(slider1LabelElm);
+		slider1LabelElm.textContent = 'Slider 1:'
+		slider1Elm.type = 'range';
+		slider1Elm.id = `slider_${id}_1`;
+		slider1Elm.setAttribute('name', slider1Elm.id);
+		slider1Elm.setAttribute('min', 0);
+		slider1Elm.setAttribute('max', 4096);
+		slider1Elm.value = 0;
+		slider1LabelElm.appendChild(slider1Elm);
+		slider1LabelElm.appendChild(slider1ValueElm);
+	  slider1ValueElm.id = `value_${id}_1`;
+		slider1ValueElm.textContent = 0;
+		slider1LabelElm.setAttribute('for', slider1Elm.id);
+		slider1Elm.addEventListener('input', (e) => {
+			slider1ValueElm.textContent = slider1Elm.value;
+		});
+
+		fsElm.appendChild(slider2LabelElm);
+		slider2Elm.type = 'range';
+		slider2Elm.id = `slider_${id}_2`;
+		slider2Elm.setAttribute('name', slider2Elm.id);
+		slider2Elm.setAttribute('min', 0);
+		slider2Elm.setAttribute('max', 4096);
+		slider2Elm.value = 0;
+		slider2LabelElm.textContent = 'Slider 2:';
+		slider2LabelElm.appendChild(slider2Elm);
+		slider2LabelElm.appendChild(slider2ValueElm);
+	  slider2ValueElm.id = `value_${id}_2`;
+		slider2ValueElm.textContent = '0';
+		slider2LabelElm.setAttribute('for', slider2Elm.id);
+		slider2Elm.addEventListener('input', (e) => {
+			slider2ValueElm.textContent = slider2Elm.value;
+		});
+	
+		return fsElm;
+}
+
+function createSliderGroup2(id) {
+  const clone = document.body.querySelector('#slider-pair').content.cloneNode(true);
+	let legendElm = clone.querySelector('fieldset > legend > input[type=checkbox]'),
+		labelElms = clone.querySelectorAll('fieldset > label'),
+		slider1Elm = labelElms[0].querySelector('input'),
+		slider1ValueElm = labelElms[0].querySelector('span'),
+		slider2Elm = labelElms[1].querySelector('input'),
+		slider2ValueElm = labelElms[1].querySelector('span');
+
+	slider1Elm.id = `slider_${id}_1`;
+	labelElms[0].setAttribute('for', slider1Elm.id);
+	slider1Elm.setAttribute('name', slider1Elm.id);
+	slider1ValueElm.id = `value_${id}_1`;
+	slider1Elm.addEventListener('input', (e) => {
+		slider1ValueElm.textContent = slider1Elm.value;
+	});
+
+	slider2Elm.id = `slider_${id}_2`;
+	labelElms[1].setAttribute('for', slider2Elm.id);
+	slider2Elm.setAttribute('name', slider2Elm.id);
+	slider2ValueElm.id = `value_${id}_2`;
+	slider2Elm.addEventListener('input', (e) => {
+		slider2ValueElm.textContent = slider2Elm.value;
+	});
+  return clone;
 }
 
 function updateSliderValue(sliderId, valueId) {
@@ -58,15 +129,17 @@ function sendData() {
 	}
 }
 
-document.getElementById('sync1').addEventListener('change', () => syncSliders('slider1', 'slider2', 'sync1'));
-document.getElementById('sync3').addEventListener('change', () => syncSliders('slider3', 'slider4', 'sync3'));
-document.getElementById('sync5').addEventListener('change', () => syncSliders('slider5', 'slider6', 'sync5'));
+document.body.querySelector('form#myForm').appendChild(createSliderGroup2('g1'));
 
-document.getElementById('slider1').addEventListener('input', () => updateSliderValue('slider1', 'slider1Value'));
-document.getElementById('slider2').addEventListener('input', () => updateSliderValue('slider2', 'slider2Value'));
-document.getElementById('slider3').addEventListener('input', () => updateSliderValue('slider3', 'slider3Value'));
-document.getElementById('slider4').addEventListener('input', () => updateSliderValue('slider4', 'slider4Value'));
-document.getElementById('slider5').addEventListener('input', () => updateSliderValue('slider5', 'slider5Value'));
-document.getElementById('slider6').addEventListener('input', () => updateSliderValue('slider6', 'slider6Value'));
+// document.getElementById('sync1').addEventListener('change', () => syncSliders('slider1', 'slider2', 'sync1'));
+// document.getElementById('sync3').addEventListener('change', () => syncSliders('slider3', 'slider4', 'sync3'));
+// document.getElementById('sync5').addEventListener('change', () => syncSliders('slider5', 'slider6', 'sync5'));
 
-document.getElementById('sendToServer').addEventListener('change', sendData);
+// document.getElementById('slider1').addEventListener('input', () => updateSliderValue('slider1', 'slider1Value'));
+// document.getElementById('slider2').addEventListener('input', () => updateSliderValue('slider2', 'slider2Value'));
+// document.getElementById('slider3').addEventListener('input', () => updateSliderValue('slider3', 'slider3Value'));
+// document.getElementById('slider4').addEventListener('input', () => updateSliderValue('slider4', 'slider4Value'));
+// document.getElementById('slider5').addEventListener('input', () => updateSliderValue('slider5', 'slider5Value'));
+// document.getElementById('slider6').addEventListener('input', () => updateSliderValue('slider6', 'slider6Value'));
+
+// document.getElementById('sendToServer').addEventListener('change', sendData);
--- server.js
+++ server.js
@@ -2,26 +2,71 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const WebSocket = require('ws');
+const url = require('url');
 
-const port = 3000;
+const port = process.argv[2] || 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
+// maps file extention to MIME types
+// full list can be found here: https://www.freeformatter.com/mime-types-list.html
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.zip': 'application/zip',
+  '.doc': 'application/msword',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.ttf': 'application/x-font-ttf',
+};
+
+const server = http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+
+  // extract URL path
+  // Avoid https://en.wikipedia.org/wiki/Directory_traversal_attack
+  // e.g curl --path-as-is http://localhost:9000/../fileInDanger.txt
+  // by limiting the path to current directory only
+  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
+  let pathname = path.join(__dirname, sanitizePath);
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory, then look for index.html
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += '/index.html';
+    }
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+        const ext = path.parse(pathname).ext;
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
         res.end(data);
       }
     });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+  });
 });
-
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
@@ -42,6 +87,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-server.listen(port, () => {
+server.listen(parseInt(port), () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
